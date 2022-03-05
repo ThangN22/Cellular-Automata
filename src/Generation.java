369c530aa71feb9676f4ae2d73cdabda62eb0bef
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-03-04 18:58:56 -0600

    Create comments to create javadoc

--- src/Generation.java
+++ src/Generation.java
@@ -1,13 +1,21 @@
 import java.util.Arrays;
-
+/** 
+ * This class represents a row of cells at a fixed time. Each generation encapsulates a boolean array that represents the cell states.
+ * The cells are indexed from left to right starting at zero, so the state of the first cell is cellStates[0], the state of the second 
+ * cell is cellStates[1] and so on. 
+ * @author Thang Nguyen
+ *
+ */
 public class Generation {
 
 	private boolean[] cellStates;
 
-	// Written during tutor hours 02/18/2022 alongside Tommy Pham and Halyn Nguyen
+	/**
+	 * Create a generation with one cell for each element in the given array. The state of each cell is specified by the value of the corresponding element.
+	 * @param states boolean states, true or false, to be added into boolean[] cellStates
+	 */
 	public Generation(boolean... states) {
 		
-// I OMITTED CELLSTATES == NULL AND EVERTHING WORKED FOR SOME REASON
 		if (states == null || states.length <= 0) {
 			cellStates = new boolean[1]; // initiation of the cellStates
 			cellStates[0] = false;
@@ -21,13 +29,17 @@ public class Generation {
 		}
 	}
 
+	/**
+	 * Create a generation with one cell for each character in the given String. 
+	 * @param states A string of two characters to be read into true and false states. These states will then be added into boolean[] cellStates.
+	 * @param trueSymbol Used to compare with a corresponding cell. If equal, the cell is true. Otherwise, the state is false.
+	 */
 	public Generation(String states, char trueSymbol) {
 		if (states == null || states.length() == 0) {
 			cellStates = new boolean[1]; // initiation of the cellStates
 			cellStates[0] = false;
 		}
 		
-		// REMEMBER THIS ELSE!!!
 		else {
 			cellStates = new boolean[states.length()]; // initiation of the cellStates
 
@@ -40,10 +52,19 @@ public class Generation {
 		}
 	}
 
+	/**
+	 * Return the state of the cell with the given index.
+	 * @param idx the given index
+	 * @return the state of the cell
+	 */
 	public boolean getState(int idx) { // Make a copy of the array for immutability
 		return cellStates[idx];
 	}
 
+	/**
+	 * Return an array with all of the cell states
+	 * @return
+	 */
 	public boolean[] getStates() {
 
 		boolean[] copyOfCellStates = Arrays.copyOf(cellStates, cellStates.length);
@@ -51,6 +72,12 @@ public class Generation {
 		return copyOfCellStates;
 	}
 
+	/**
+	 * Return a String representation of the cell states using falseSymbol and trueSymbol as the symbols false and true, respectively.
+	 * @param falseSymbol symbol for false
+	 * @param trueSymbol symbol for true
+	 * @return String a string representation of the cell states using falseSymbol and trueSymbol
+	 */
 	public String getStates(char falseSymbol, char trueSymbol) {
 		String stanTwice = ""; // This creates a string to be appended with the parameters falseSymbol and true Symbol
 		for (int idx = 0; idx < this.cellStates.length; ++idx) {
