c78c9d6e1f6a3dc3cce7e75e05b57b543b451d6a
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-03-04 18:40:54 -0600

    Copy Generation.java from project 1

--- src/Generation.java
+++ src/Generation.java
@@ -1,4 +1,71 @@
+import java.util.Arrays;
 
 public class Generation {
+
+	private boolean[] cellStates;
+
+	// Written during tutor hours 02/18/2022 alongside Tommy Pham and Halyn Nguyen
+	public Generation(boolean... states) {
+		
+// I OMITTED CELLSTATES == NULL AND EVERTHING WORKED FOR SOME REASON
+		if (states == null || states.length <= 0) {
+			cellStates = new boolean[1]; // initiation of the cellStates
+			cellStates[0] = false;
+		} 
+		else {
+			cellStates = new boolean[states.length]; // initiation of the cellStates
+
+			for (int idx = 0; idx < cellStates.length; ++idx) {
+				cellStates[idx] = states[idx];
+			}
+		}
+	}
+
+	public Generation(String states, char trueSymbol) {
+		if (states == null || states.length() == 0) {
+			cellStates = new boolean[1]; // initiation of the cellStates
+			cellStates[0] = false;
+		}
+		
+		// REMEMBER THIS ELSE!!!
+		else {
+			cellStates = new boolean[states.length()]; // initiation of the cellStates
+
+			for (int idx = 0; idx < cellStates.length; ++idx) {
+				if (states.charAt(idx) == trueSymbol) {
+					cellStates[idx] = true;
+				} else
+					cellStates[idx] = false;
+			}
+		}
+	}
+
+	public boolean getState(int idx) { // Make a copy of the array for immutability
+		return cellStates[idx];
+	}
+
+	public boolean[] getStates() {
+
+		boolean[] copyOfCellStates = Arrays.copyOf(cellStates, cellStates.length);
+
+		return copyOfCellStates;
+	}
+
+	public String getStates(char falseSymbol, char trueSymbol) {
+		String stanTwice = ""; // This creates a string to be appended with the parameters falseSymbol and true Symbol
+		for (int idx = 0; idx < this.cellStates.length; ++idx) {
+			if (cellStates[idx] == true) {
+				stanTwice += trueSymbol;
+			} else if (cellStates[idx] == false) {
+				stanTwice += falseSymbol;
+			}
+
+		}
+		return stanTwice;
+	}
+
+	public int size() {
+		return cellStates.length;
+	}
 
 }
