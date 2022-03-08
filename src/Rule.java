506c61fac36ab2c0106360f0b9cd81f4b067c6fc
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-03-08 14:19:55 -0600

    Satisfy abstract requirements for the class

--- src/Rule.java
+++ src/Rule.java
@@ -1,20 +1,10 @@
 
-public class Rule {
+public abstract class Rule {
 	private int ruleNum;
-	private String binaryRuleNum;
+	// Add variables as needed
 
-	public Rule(int ruleNum) {
-		if (ruleNum > 255) {
-			this.ruleNum = 255;
-		} else if (ruleNum < 0) {
-			this.ruleNum = 0;
-		} else if (ruleNum >= 0 && ruleNum <= 255) {
-			this.ruleNum = ruleNum;
-		}
-
-		String binaryString = Integer.toBinaryString(ruleNum); // Receives integer and converts to String
-		binaryRuleNum = String.format("%8s", binaryString).replace(' ', '0'); // adds in the zeros where they should be
-
+	protected Rule(int ruleNum) throws RuleNumException {
+		this.ruleNum = ruleNum;
 	}
 
 	public int getRuleNum() {
@@ -22,115 +12,10 @@ public class Rule {
 		return ruleNum;
 	}
 
-	public static boolean[] getNeighborhood(int idx, Generation gen) {
+	public abstract boolean[] getNeighborhood(int idx, Generation gen);
+	
 
-		boolean[] booleanGen = new boolean[3];
-		if (gen.size() == 2) { // checks if the Generation parameter is less than 3
-			if (idx == 0) {
-				booleanGen[0] = gen.getState(idx);
-				booleanGen[1] = gen.getState(idx);
-				booleanGen[2] = gen.getState(idx + 1);
-				return booleanGen;
-			} 
-			else if (idx == 1) {
-				booleanGen[0] = gen.getState(idx - 1);
-				booleanGen[1] = gen.getState(idx);
-				booleanGen[2] = gen.getState(idx);
-				return booleanGen;
-			}
-			
-		}
-		if (gen.size() == 1) { // checks if the Generation parameter is less than 2
-			booleanGen[0] = gen.getState(idx);
-			booleanGen[1] = gen.getState(idx);
-			booleanGen[2] = gen.getState(idx);
-			return booleanGen;
-		}
-
-		// Circular condition
-		if (idx == 0) { // Accounts for the index being in the beginning of the gen array
-			booleanGen[0] = gen.getState(gen.size() - 1);
-			booleanGen[1] = gen.getState(idx);
-			booleanGen[2] = gen.getState(idx + 1);
-			return booleanGen;
-		}
-
-		else if (idx == gen.size() - 1) { // Accounts for the index being in the end of the gen array
-			booleanGen[0] = gen.getState(idx - 1);
-			booleanGen[1] = gen.getState(idx);
-			booleanGen[2] = gen.getState(0);
-			return booleanGen;
-		}
-
-		else { // Accounts for ever other situation where the index is somewhere in the center
-				// of the gen array
-			booleanGen[0] = gen.getState(idx - 1);
-			booleanGen[1] = gen.getState(idx);
-			booleanGen[2] = gen.getState(idx + 1);
-			return booleanGen;
-		}
-
-	}
-
-	public boolean evolve(boolean[] neighborhood) {
-		/*
-		 * Take the binary in String format and convert it to a boolean array of length
-		 * 8. The use that to dictate the evolution
-		 */
-
-		boolean[] binaryArray1 = new boolean[8];
-
-		for (int idx = 0; idx < binaryArray1.length; ++idx) {
-			if (binaryRuleNum.charAt(idx) == '1') {
-				binaryArray1[idx] = true;
-			} else if (binaryRuleNum.charAt(idx) == '0') {
-				binaryArray1[idx] = false;
-			}
-		}
-
-		// Account for  the 8 neighborhood configurations
-
-		// Branch one
-		if (neighborhood[0] == true) {
-			if (neighborhood[1] == true) {
-				if (neighborhood[2] == true) {
-					return binaryArray1[0];
-				} else if (neighborhood[2] == false) {
-					return binaryArray1[1];
-				}
-			}
-
-			else if (neighborhood[1] == false) {
-				if (neighborhood[2] == true) {
-					return binaryArray1[2];
-				} else if (neighborhood[2] == false) {
-					return binaryArray1[3];
-				}
-			}
-		}
-
-		// Branch Two
-		else if (neighborhood[0] == false) {
-			if (neighborhood[1] == true) {
-				if (neighborhood[2] == true) {
-					return binaryArray1[4];
-				} else if (neighborhood[2] == false) {
-					return binaryArray1[5];
-				}
-			}
-
-			else if (neighborhood[1] == false) {
-				if (neighborhood[2] == true) {
-					return binaryArray1[6];
-				} else if (neighborhood[2] == false) {
-					return binaryArray1[7];
-				}
-			}
-
-		}
-
-		return false;
-	}
+	public abstract boolean evolve(boolean[] neighborhood);
 
 	public Generation evolve(Generation gen) {
 		String evolvedGenString = "";
@@ -144,4 +29,24 @@ public class Rule {
 
 		return evolved; 
 	}
+	
+	public abstract String ruleTableString(char falseSymbol, char trueSymbol);
+	
+	/**
+	 * for getRadius, perhaps try absolute value of % instead of using floormod? 
+	 * abstract classes do not have a method body, but rather has a semicolon
+	 */
+	public static boolean[] getNeighborhoodByRadius(int idx, int radius, Generation gen) {
+		int booleanSize = 1 + (radius * 2);
+		boolean[] cellStates = new boolean[booleanSize];
+		int genIdx = Math.floorMod(idx - radius, gen.size());
+		for (int index = 0; index < cellStates.length; ++index) {
+			
+			cellStates[index] = gen.getState(genIdx);
+			++genIdx;
+		}
+		return cellStates;
+	}
 }
+
+	
