6c8884b4099e50b56e1c42bb0d3384d7cf1496f6
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-03-08 15:18:54 -0600

    Write framwork for method bodies

--- src/TotalisticRule.java
+++ src/TotalisticRule.java
@@ -1,6 +1,26 @@
 
 public class TotalisticRule extends Rule {
-	public TotalisticRule() {
+	protected TotalisticRule(int ruleNum) throws RuleNumException {
+		super(ruleNum);
+		int min = 0;
+		int max = 63; 
 		
+		if (ruleNum < 0 || ruleNum > max) {
+			throw new RuleNumException(min, max);
+		}
+	}
+	
+	public boolean evolve(int idx, Generation gen) {
+		return false; // stub
+	}
+	
+	public boolean[] getNeighborhood(int idx, int Generation) {
+		boolean[] baka = {false};
+		return baka;
+	}
+	
+	public String ruleTableString(char falseSymbol, char trueSymbol) {
+		String baka = "";
+		return baka;
 	}
 }
