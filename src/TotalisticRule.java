000aae0dcad39ce6e0c275ecbcd0a63b8158d297
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-03-08 22:12:38 -0600

    Complete evolve method

--- src/TotalisticRule.java
+++ src/TotalisticRule.java
@@ -10,17 +10,52 @@ public class TotalisticRule extends Rule {
 		}
 	}
 	
-	public boolean evolve(int idx, Generation gen) {
-		return false; // stub
+	
+	@Override
+	public boolean[] getNeighborhood(int idx, Generation gen) {
+		return super.getNeighborhoodByRadius(idx, 2, gen);  // check for 5 spaces, so use two as the radius param.
 	}
 	
-	public boolean[] getNeighborhood(int idx, int Generation) {
-		boolean[] baka = {false};
-		return baka;
-	}
-	
+	@Override
 	public String ruleTableString(char falseSymbol, char trueSymbol) {
 		String baka = "";
 		return baka;
+	}
+
+	@Override
+	public boolean evolve(boolean[] neighborhood) {
+		// TODO Auto-generated method stub
+		boolean[] binaryArray1 = new boolean[6];
+		//int[] numTrue = {5, 4, 3, 2, 1, 0};
+		int trueCount = 0;
+		String binaryString = Integer.toBinaryString(getRuleNum()); // Receives integer and converts to String
+		binaryString = String.format("%6s", binaryString).replace(' ', '0'); // adds in the zeros where they should be
+		
+		for (int idx = 0; idx < binaryArray1.length; ++idx) {
+			if (binaryString.charAt(idx) == '1') {
+				binaryArray1[idx] = true;
+			} else if (binaryString.charAt(idx) == '0') {
+				binaryArray1[idx] = false;
+			}
+		}
+		for (int idx = 0; idx < neighborhood.length; ++idx) {
+			if (neighborhood[idx] == true) {
+				++trueCount;
+			}
+		}
+		if (trueCount == 5)
+			return binaryArray1[0];
+		if(trueCount ==4) 
+			return binaryArray1[1];
+		if(trueCount == 3)
+			return binaryArray1[2];
+		if(trueCount == 2) 
+			return binaryArray1[3];
+		if(trueCount ==1)
+			return binaryArray1[4];
+		if(trueCount ==0)
+			return binaryArray1[5];
+		
+		return false;
 	}
 }
