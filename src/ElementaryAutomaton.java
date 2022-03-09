4f82bdcc6fd13ceab86a5f4349652f5bd02917ea
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-03-09 14:28:40 -0600

    Complete ElementaryAutomaton.java

--- src/ElementaryAutomaton.java
+++ src/ElementaryAutomaton.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
 
 public class ElementaryAutomaton extends Automaton {
+
+	protected ElementaryAutomaton(int ruleNum, Generation initial) throws RuleNumException {
+		super(ruleNum, initial);
+		
+	}
+	
+	protected ElementaryAutomaton(String filename) throws IOException, RuleNumException {
+		super(filename);
+	}
+
+	protected Rule createRule(int ruleNum) {
+		Rule rule = null;
+		try {
+		rule = new ElementaryRule(ruleNum);
+		return rule;
+		}
+		catch (RuleNumException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
