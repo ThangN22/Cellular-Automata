7582485d153b8cbd4ed90851026d173ed86214bc
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-03-09 20:12:36 -0600

    Complete TotalisticAutomaton.java

--- src/TotalisticAutomaton.java
+++ src/TotalisticAutomaton.java
@@ -1,4 +1,27 @@
+import java.io.IOException;
 
 public class TotalisticAutomaton extends Automaton {
+
+	protected TotalisticAutomaton(int ruleNum, Generation initial) throws RuleNumException {
+		super(ruleNum, initial);
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected TotalisticAutomaton(String filename) throws IOException, RuleNumException {
+		super(filename);
+	}
+
+	@Override
+	protected Rule createRule(int ruleNum) {
+		Rule rule = null;
+		try {
+		rule = new TotalisticRule(ruleNum);
+		return rule;
+		}
+		catch (RuleNumException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
